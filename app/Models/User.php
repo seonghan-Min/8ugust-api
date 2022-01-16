ace913b515f2308fc9cb591a912e181514dcac31
shmin <gks831@naver.com>
2022-01-16 22:21:38 +0900

    [#0004] JWT 구현

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,11 +12,7 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 class User extends Authenticatable implements JWTSubject {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    protected $connection = 'mysql';
     protected $fillable = [
         'name',
         'email',
